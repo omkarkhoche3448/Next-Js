b0825b27060166414b26c26b3c67c566850aff8a
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-10-06 00:14:30 +0530

    serverside db interactions

--- next-serverside3/app/page.tsx
+++ next-serverside3/app/page.tsx
@@ -1,20 +1,54 @@
 import axios from "axios";
 
-async function getUser() {
-  const res = await axios.get("http://localhost:3000/api/user");
-  // console.log(res.data);
-  return res.data;
+// async function getUser() {
+//   const res = await axios.get("http://localhost:3000/api/user");
+//   // console.log(res.data);
+//   return res.data;
+// }
+
+
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function getLastUser() {
+  try {
+    const lastUser = await client.user.findFirst({
+      orderBy: {
+        id: "desc",
+      },
+      select: {
+        userName: true, 
+        password: true,   
+      },
+    });
+    return lastUser;
+  } catch (error) {
+    console.error("Error fetching last user:", error);
+    return null;
+  } finally {
+    await client.$disconnect();
+  }
 }
 
 export default async function Home() {
-  const userDetails = await getUser();
+  const userDetails = await getLastUser();
+  // const userDetails = await getUser();
+  // console.log("User:", userDetails); 
+
   return (
     <div>
       Hello there!
       <br />
-      userName:{userDetails?.name}
-      <br />
-      password:{userDetails?.password}
+      {userDetails ? (
+        <>
+          UserName: {userDetails.userName}
+          <br />
+          Password: {userDetails.password}
+        </>
+      ) : (
+        <p>No user found.</p>
+      )}
     </div>
   );
 }
